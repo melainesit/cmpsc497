1ffb7a28a1af5c690e24128e0956bb7339e67694
Melaine Sit <mms6909@psu.edu>
2022-02-27 12:47:23 -0800

    request 1,2,3 work

--- client.py
+++ client.py
@@ -1,27 +1,165 @@
 
 import socket
+import json
+import threading
+from _thread import *
+import os
+
+
+# message #5
+def File_Chunk_Request(conn, addr):
+    # Parse the message
+    # Find the chunk
+    # Send the chunk over conn
+    receive = conn.recv(4096).decode()
+    data = json.loads(receive)
+    chunknum = data["chunk"]
+    offset = chunknum*1024
+    fileName = data["fileName"]
+    f.open(fileName,"r")
+    f.seek(offset)
+    chunkfile = f.read(2014)
+    conn.sendto(str.encode(chunkfile),addr)
+    return 0 
+    # return -1 if failed
+
+
+def recv_msg(sock):
+    #receive = socket.recv(4096).decode('utf-8')
+    receive = sock.recv(4096).decode()
+    print("this is rec: "+ str(receive))
+    #socket.connect((HOST,PORT))
+    data = json.loads(receive)
+    return data
+
+def send_msg(sock, dic):
+    send = json.dumps(dic)
+    sock.send(str.encode(send))
+    return 0
+
+def start_listener(sock):
+    sock.listen()
+    while True:
+        conn, addr = sock.accept()
+        print("Connected by ", addr)
+        start_new_thread(File_Chunk_Request, (conn, addr))
+
+
 
 # localhost
 HOST = '127.0.0.1'  
 # Port to listen on    
-PORT = 65007
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+PORT = 65026
+LISTEN_PORT = 65016
+print("Waiting for connection")
 #with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-s.connect((HOST,PORT))
-    #s.sendall(b'Hello world')
-data = s.recv(1024)
+#sock.listen()
+sListen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sListen.bind((HOST, LISTEN_PORT))
+start_new_thread(start_listener, (sListen,))
 
-#print('Receiveed', str(data))
 
-while True:
-    line = input("Send the Server message:")
-    if line == 'exit':
-        break
 
-    s.send(str.encode(line))
-    data = s.recv(1024)
+# Peer format for messages:
 
-    print(data.decode('utf-8'))
+# Message 1
+# {
+#   message: 1,
+#   files: [
+#       (fileName, fileSize)
+#   ]
+# }
 
-s.close()
+# Message 2
+# {
+#   message: 1,
+# }
+
+# Message 3
+# {
+#   message: 1,
+#   fileName: fileName
+# }
+
+# Message 4
+#{
+#   message: 4,
+#   file: "filename",
+#   chunk: chunkNumber
+# }
+
+# Message 5
+#{
+#   message: 5,
+#   fileName: "filename",
+#   chunk: chunkNumber
+# }
+ansr = "y"
+while ansr=="y":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    #data = s.recv(4096).decode()
+    message = input("What message would you like to send: ")
+    message = message.strip()
+    msgdict = {}
+    
+    if message == "1":
+        mylist = []
+        ans = "y"
+        while ans == "y":
+            files = input("What file do you want to share: ")
+            files = files.strip()
+            #f.open(files,"r")
+            size = os.path.getsize("./"+files)
+            tup = (files, size)
+            mylist.append(tup)
+            ans = input("Would you like to add another file? (y/n): ")
+            ans = ans.strip()
+        msgdict["message"] = 1
+        msgdict["files"] = mylist
+        msgdict["lPort"] = LISTEN_PORT
+        send_msg(s,msgdict)
+        data = recv_msg(s)
+        if data["success"] != 1:
+            print("An error has occurred")
+    
+    
+    if message == "2":
+        msgdict["message"] = 2
+        send_msg(s, msgdict)
+        data = recv_msg(s)
+        #receive = s.recv(4096).decode()
+        #data = json.loads(receive)
+
+    if message == "3":
+        fileName = input("What file are you looking for:")
+        fileName = fileName.strip()
+        msgdict["message"] = 3
+        msgdict["fileName"] = fileName
+        send_msg(s, msgdict)
+        data = recv_msg(s)
+
+    if message == "5":
+        fileName = input("What file: ")
+        fileName = fileName.strip()
+        chunk = input("What chunk number: ")
+        chunk = chunk.strip()
+        msgdict["message"] = 5
+        msgdict["fileName"] = fileName
+        msgdict["chunk"] = chunk
+        # send the message to the peer (how does it even know what peer)
+        send_msg(s, msgdict)
+        # receive the chunk 
+        recv_msg(s)
+        # reformat the message
+        msgdict["message"] = 4
+        # send the message to the server
+        send_msg(s, msgdict)
+        # should receive the message that it was successful
+        data = recv_msg(s)
+        if data["success"] != 1:
+            print("An error has occurred")
+
+    s.close()
+    ansr = input("Do you want to send another meesage? (y/n): ")
+    ansr = ansr.strip()
--- server.py
+++ server.py
@@ -3,47 +3,156 @@ import socket
 import threading
 import os
 from _thread import *
+import json
+import math
 
-ThreadCount = 0
+# message #1
+# The server is being told what files the peer wants to share with the network
+def register_request(conn, peer, files, lPort):
+    peerKey = str(peer[0])+":"+str(lPort)
+    for pFile in files:
+        # checks if the file name exists in the dictinary 
+        # if so, then add the list of chunks for that peer 
+        if pFile[0] in fileDict:
+            # why would we need at add one at the end??
+            fileDict[pFile[0]][peerKey] = list(range(1, pFile[1]+1))
+        
+        # file name did not exist in the dictionary so add it in
+        else:
+            # add the peer and its chunks          
+            fileDict[pFile[0]] = {peerKey: list(range(1, pFile[1]+1))}
+        # send to peer that the specific file was successfully added to the server
+        conn.sendto(str.encode(json.dumps({"message":1, "file":pFile[0], "success":1})), peer)
+    #conn.sendto(str.encode("Files successfully added"), peer)
+    print("this is dict: " + str(fileDict))
+    return 0
+
+# message #2: 
+# requests the file list
+# send the list length anf for each file, the file name and the length of that file to the peer
+# takes in the peer ip address and port
+def file_list_request(conn, peer):
+    mydict = {}
+    mydict["message"] = 2
+    mydict["length"] = len(fileDict.keys())
+    mydict["files"] = {}
+    for key in fileDict:
+        mydict["files"][key] = fileDict.get(key)
+    print("this is 2y: " + str(mydict))
+    y = json.dumps(mydict)
+    #mydict["length"] = len(mydict.key())
+    #length = str(len(mydict.keys()))
+    #package = length + ' ' + y
+    conn.sendto(str.encode(y),peer)
+    return 0
+
+# message #3
+# Requested: The IP end-points of the peers containting the requested file
+def file_locations_request(conn, peer, filename):
+    if filename in fileDict:
+        # gets the peers and the chunks
+        # where did my.dict come from
+        newdict = fileDict.get(filename)
+        # the number of endpoints
+        newdict["length"] = len(newdict.keys())
+        #length = len(newdict.keys())
+        print("this is 3y: " + str(newdict))
+        y = json.dumps(newdict)
+        #package = length + " " + y
+        conn.sendto(str.encode(y),peer)
+        return 0
+    else:
+        return -1
+    # else none if the filename doesnt exist?
+
+# message #4
+# The server is being notified when a peer receives a new chunk of the file and becomes a source 
+# (of that chunk) for other peers
+def chunk_register_request(conn, peer, pFile, chunk):
+    peerKey = str(peer[0])+":"+str(peer[1])
+    if fileDict[pFile][peerKey]:
+        fileDict[pFile][peerKey].append(chunk)
+    else: 
+        fileDict[pFile][peerKey] = [chunk]
+    conn.sendto(str.encode("Chunk successfully added"), peer)
+    return 0
+
+
+# Peer format for messages:
+
+# Message 1
+# {
+#   message: 1,
+#   lPort: port_number,
+#   files: [
+#       (fileName, fileSize)
+#   ]
+# }
+
+# Message 2
+# {
+#   message: 1,
+#   files: [
+#       (fileName, fileSize)
+#   ]
+# }
+
+# Message 3
+# {
+#   message: 1,
+#   fileName: fileName
+#   ]
+# }
+
+# Message 4
+#{
+#   message: 4,
+#   file: "filename",
+#   chunk: chunkNumber
+# }
+
+# handles requests from the individual client within a thread
+def handle_conn(conn, peer):
+    data = conn.recv(4096).decode()
+    data = json.loads(data)
+    message = data["message"]
+    if message == 1:
+        files = []
+        for peerFile in data["files"]:
+            # math.ceil(data[peerFile[1]] / 1024) ?? 
+            # i guess each chunk is 1024 bytes
+            files.append((peerFile[0], math.ceil(peerFile[1] / 1024)))
+        register_request(conn, peer, files, data["lPort"])
+    # done
+    if message == 2:
+        file_list_request(conn, peer)
+    # 
+    if message == 3:
+        filename = data["fileName"]
+        file_locations_request(conn, peer, filename)
+    if message == 4:
+        pfile = data["fileName"]
+        chunk = data["chunk"]
+        chunk_register_request(conn, peer, pFile, chunk)
+    conn.close()
+    
+# a dictionary where the key is the file name. Each file name has a dictionary. in that dictionary it will have the peer as a key 
+# and a list of chunks it has. the peer will be a tuple of ip addr and port
+# ex: {filename: {peer1: [1,3,5,7,8]}}
+fileDict = {}
 
 # localhost
 HOST = '127.0.0.1'  
 # Port to listen on    
-PORT = 65007
+PORT = 65026
     
-# main
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-s.bind((HOST,PORT))
-s.listen()
-    #conn, addr = s.accept()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((HOST,PORT))
+print("Waiting for a connection")
+sock.listen()
 
-# handles requests from the individual client by a thread
-def socket_target(conn):
-    conn.send(str.encode("Hello Client!"))
-    while True:
-        data = conn.recv(1024)
-        reply = 'Server sent this message: ' + data.decode('utf-8') 
-        if not data:
-            break
-        conn.sendall(str.encode(reply))
-    conn.close()
-
-#with conn:
-#        print('Connected by', addr)
-#        while True:
-    #        data = conn.recv(1024)
-    #        if not data:
-    #            break
-            # ensures that all data is sent
-    #        conn.sendall(data)
-    
 while True:
-    conn, addr = s.accept()
+    conn, addr = sock.accept()
     print("Connected by ", addr)
-        #socket_listen.append(conn)
-        #threading.Thread(target = socket_target, args = (conns,)).start
-    start_new_thread(socket_target, (conn, ))
-    ThreadCount +=1
-    print("Thread Number: " + str(ThreadCount))
-s.close()
+    start_new_thread(handle_conn, (conn, addr))
+sock.close()
