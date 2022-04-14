652f51e0dc7e9700eceb09b101342114692897b7
Melaine Sit <mms6909@psu.edu>
2022-04-13 21:43:47 -0700

    working solution

--- client.py
+++ client.py
@@ -6,252 +6,51 @@ from _thread import *
 import os
 import sys
 import shutil
+from datetime import datetime
 
-# Message 1 format
-# {
-#   message: 1,
-#   files: [
-#       (fileName, fileSize)
-#   ]
+
+
+# message #7 to send a message for lab 2
+# Message 7 format
+#{
+#   message: 7,
+#   Message: "comment",
+#   Timestamp: timestamp
 # }
-def message1():
+
+# create the message to send out. Will go to the server first
+def write(sender, comment, timestamp):
+    # create a socket to send the message to the server
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
-    # list of files that will be shared with the server
-    mylist = []
-    # if the client has to files to share then it just passes through the message 1. 
-    if len(sys.argv) <=4:
-        print("This peer shares no files with the server")
-        msgdict = {"message": 1}
-        msgdict["files"] = mylist
-        msgdict["lPort"] = LISTEN_PORT
-    else:
-        # get the files from the command line arguments
-        for arg in range(4, len(sys.argv)):
-            size = os.path.getsize(CLIENT_DIR+sys.argv[arg])
-            # tuples of the file name and the size of the file
-            tup = (sys.argv[arg], size)
-            mylist.append(tup)
-        # format the message to send
-        msgdict = {"message": 1}
-        msgdict["files"] = mylist
-        msgdict["lPort"] = LISTEN_PORT
-    send_msg(sock,msgdict)
+    # create the dictionary
+    msgdict = {"message": 1}
+    # sender is just a name
+    msgdict["sender"] = sender
+    msgdict["comment"] = comment
+    msgdict["timestamp"] = timestamp
+
+    # add in sender
+    # send and receive the data
+    send_msg(sock, msgdict)
     data = recv_msg(sock)
     print(data)
-    #if data["success"] != 1:
-    #    print("An error has occurred")
     sock.close()
-    return data
 
-
-# Message 2 format
-# {
-#   message: 2,
-# }
-def message2():
+def request_comments():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
-    # format message to send
+    # create the dictionary
     msgdict = {"message": 2}
     send_msg(sock, msgdict)
     data = recv_msg(sock)
-    print(data)
-    # a list of file names and the peers that have chunks of those files
-    for lfile in data["files"].keys():
-        fileList[lfile] = data["files"][lfile]
-    #print("This is fileList: " + str(fileList))
+    print("These are currently the messages: " + str(data["comment"]))
+    #print(type(data))
+    #return data
+    #print("this is request:" + str(data))
     sock.close()
-    return 0
-
-# Message 3 format
-# {
-#   message: 3,
-#   fileName: fileName
-# }
-# Asks the server for the list of files
-# Returns the number of files in the list; and for each file, a file name and a file length.
-def message3():
-    # get the list of files
-    message2()
-    # just the file names from fileList
-    listoffiles = []
-    for key in fileList.keys():
-        listoffiles.append(key)
-
-    # offer that list to the user and ask them to pick off that list
-    print("Here are a list of files to choose from:" )
-    # for error checking to make sure that the user picks a number from the list
-    tempindexlist = []
-    for i in range(len(listoffiles)):
-        fi = listoffiles[i]
-        i+=1
-        tempindexlist.append(i)
-        print(str(i) + ".\t" + str(fi))
-
-    fileNum = int(input("What file are you looking for: ").strip())
-    
-    # checks if the fileName exists in the list of files
-    while fileNum not in tempindexlist:
-        print("File not found, try again")
-        fileNum = int(input("What file do you want: ").strip())
-    fileName = listoffiles[fileNum-1]
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((HOST, PORT))
-    #format message 3
-    msgdict = {"message": 3}
-    msgdict["fileName"] = fileName
-    #print("this is msgdict3: " + str(msgdict))
-    send_msg(sock, msgdict)
-    data = recv_msg(sock)
-    print(data)
-    sock.close()
-    return data
-
-# Message 4 format
-#{
-#   message: 4,
-#   fileName: "filename",
-#   chunk: chunkNumber
-# }
-# Tells the server when a peer receives a new chunk of the file and becomes a source (of that chunk) for other peers.
-def message4(fileName, chunk):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((HOST, PORT))
-    # format message 4
-    msgdict = {"message": 4}
-    msgdict["fileName"] = fileName
-    msgdict["chunk"] = chunk
-    msgdict["lPort"] = LISTEN_PORT
-    send_msg(sock, msgdict)
-    data = recv_msg(sock)
-    print(data)
-
-    # should receive the message that it was successful
-    if data["success"] != 1:
-        print("An error has occurred")
-    sock.close()
-    return 0
-
-# Message 5 format
-#{
-#   message: 5,
-#   fileName: "filename",
-#   chunk: chunkNumber
-# }
-def message5():
-    # total list of chunks for that file from all peers
-    listofchunks = []
-   
-    # checks if the file list has files, else run message 2
-    if len(fileList)==0:
-        message2()
-            
-    #print("This is the fileList: " + str(fileList))
-    # getting the ip addresses and chunks of a requested file
-    data3 = message3()
-
-    fileName = data3['fileName']
-
-    # goes through the chunks and combines them into a list
-    for key in fileList[fileName].keys():
-        listofchunks+=fileList[fileName][key]
-    # Removes the duplicate chunks
-    listofchunks = list(set(listofchunks))
-    # offers the list of chunks to the user and asks them to pick off that list
-    print("This is list of chunks: " + str(listofchunks))
-    chunk = int(input("What chunk number do you want: ").strip())
-    # checks if the chunk number is in the list
-    while chunk not in listofchunks:
-        print("Chunk not found or incorrectly typed, try again.")
-        chunk = int(input("What chunk number do you want: ").strip())
-
-    # run message 3 to find out what peer has that chunk
-    fileowners = []
-        
-    #print("this is data3: " + str(data3))
-    # go find a peer that has that chunk
-    for peer in data3["peers"].keys():
-        if chunk in data3["peers"][peer]:
-            fileowners.append(peer)
-
-    print("Choose one of these peers to receive the data from: ")
-    # list to keep track of indexes
-    tempindexlist = []
-    # make it easier for the peer to select
-    for i in range(len(fileowners)):
-        peer = fileowners[i]
-        i+=1
-        tempindexlist.append(i)
-        print(str(i) + ".\t" + str(peer))
-
-    #print(tempindexlist)
-    peernum = int(input("Enter which peer you want to receive data from:" ).strip())
-    # error check to see if the peer they requested was offered
-    while peernum not in tempindexlist:
-        print("Peer not found or incorrectly typed, try again.")
-        peernum = int(input("Choose one of these peers to receive the data from: ").strip())
-
-    peer = fileowners[peernum-1]
-    peer = peer.split(":")
-
-    # Ask the peer for the file chunks
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((peer[0], int(peer[1])))
-    msgdict = {}
-    msgdict["message"] = 5
-    msgdict["fileName"] = fileName
-    msgdict["chunk"] = chunk
-    msgdict["peer"] = peer
-    send_msg(sock, msgdict)
-    newdata = recv_msg(sock)
-
-    # only used for the files that i dont have and are requesting chunks from 
-    # a dictionary of files and the chunks w/chunk data
-    global chunkdatalist
-    if fileName in chunkdatalist.keys():
-        chunkdatalist[fileName][int(chunk)] = newdata["data"]
-    else:
-        chunkdatalist = {fileName: {int(chunk): newdata["data"]}}    
 
 
-    #print("This is chunkdatalist: " + str(chunkdatalist))
-
-    # writing to the file with chunks in order
-    f = open(CLIENT_DIR+fileName, "w+")
-    f.truncate(0)
-    for chunk in sorted(chunkdatalist[fileName].keys()):
-        f.write(chunkdatalist[fileName][chunk])
-    f.close()
-
-    # Tell the server that a peer got a new chunk
-    data = message4(fileName, chunk)
-    sock.close()
-    return data
-
-# message #5
-def File_Chunk_Request(conn, addr):
-
-    receive = conn.recv(4096).decode()
-    data = json.loads(receive)
-    # format a message to send back
-    senddict = {}
-    senddict["message"] = 5
-    senddict["fileName"] = data["fileName"]
-    senddict["chunk"] = data["chunk"]
-    # find out what chunk of data to send from the file
-    chunknum = data["chunk"]
-    offset = (chunknum-1)*1024
-    fileName = data["fileName"]
-    f = open(CLIENT_DIR+fileName,"r")
-    f.seek(offset)
-    chunkfile = f.read(1024)
-    senddict["data"] = chunkfile
-    y = json.dumps(senddict)
-    conn.sendto(str.encode(y),addr)
-    return 0 
-    # return -1 if failed
 
 def recv_msg(sock):
     receive = sock.recv(4096).decode('utf-8')
@@ -263,52 +62,45 @@ def send_msg(sock, dic):
     sock.send(str.encode(send))
     return 0
 
-# for peer to peer connection
-def start_listener(sock):
-    sock.listen()
-    while True:
-        conn, addr = sock.accept()
-        print("Connected by ", addr)
-        start_new_thread(File_Chunk_Request, (conn, addr))
 
+
+# the command line will be:
+# $ python3 client.py client1 65000  
 
 # localhost
 HOST = '127.0.0.1'  
 # Port to listen on 
 # server port   
-PORT = int(sys.argv[1])
-# myself port
-LISTEN_PORT = int(sys.argv[2])
-CLIENT_DIR = "./"+sys.argv[3]+"/"
-# a dictionary that has all the files and which ip address has what chunk
-fileList = {}
-# only used for the files that i dont have and are requesting chunks from 
-# a dictionary of files and the chunks w/chunk data
-chunkdatalist = {}
+PORT = int(sys.argv[2])
 
 print("Waiting for connection")
 
 # port to listen for peer to peer connection
 sListen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sListen.bind((HOST, LISTEN_PORT))
-start_new_thread(start_listener, (sListen,))
-message1()
+sListen.connect((HOST, PORT))
+
+
+
 
 while True:
-    message = input("What message would you like to send: ").strip()
+    allmessages = input("Do you want to see all the messages ? (yes/no): ").strip()
+    if allmessages == "yes":
+        request_comments()
     
-    #message1()
     
-    if message == "2":
-        message2()
+    message = input("Do you want to send a message? (yes/no): ").strip()
+    if message == "yes":
+        # actual message to send
+        #start_new_thread(request_comments, ())
+        sender = sys.argv[1]
+        comment = input("Input the message you want to send: ")
+        timestamp = datetime.now()
+        timestamp = timestamp.strftime("%d/%m/%Y %H:%M:%S")
+        start_new_thread(write, (sender, comment, timestamp))
 
-    if message == "3":
-        message3()
-
-    if message == "5":
-        message5()
-    
     # end loop
-    if message == "6":
+    if message == "no":
         break
+
+
 
--- server.py
+++ server.py
@@ -6,123 +6,135 @@ from _thread import *
 import json
 import math
 import sys
+from threading import Thread, Lock
 
-# message #1
-# The server is being told what files the peer wants to share with the network
-def register_request(conn, peer, files, lPort):
-    peerKey = str(peer[0])+":"+str(lPort)
-    if len(files) == 0:
-        conn.sendto(str.encode(json.dumps({"message":1, "file":[], "success":1})), peer)
-    else:
-        senddict = {}
-        senddict["message"] = 1
-        #senddict["file"] = []
-        tuplist = []
-        for pFile in files:
-            # checks if the file name exists in the dictinary 
-            # if so, then add the list of chunks for that peer 
-            if pFile[0] in fileDict:
-                fileDict[pFile[0]][peerKey] = list(range(1, pFile[1]+1))
-        
-            # file name did not exist in the dictionary so add it in
-            else:
-                # add the peer and its chunks          
-                fileDict[pFile[0]] = {peerKey: list(range(1, pFile[1]+1))}
-        
-            # send to peer that the specific file was successfully added to the server
-            print("This is fileDict" + str(fileDict))
-            tup = (pFile[0], "success")
-            tuplist.append(tup)
+ 
 
-        conn.sendto(str.encode(json.dumps({"message":1, "file": tuplist})), peer)
+
+def write_to_server(servSock, sender, comment, timestamp):
+    msgdict = {
+        "message": 3,
+        "sender": sender,
+        "comment": comment,
+        "timestamp": timestamp
+    }
+    y = json.dumps(msgdict)
+    servSock.sendto(str.encode(y),addr)
     return 0
 
-# message #2: 
-# requests the file list
-# send the list length anf for each file, the file name and the length of that file to the peer
-# takes in the peer ip address and port
-def file_list_request(conn, peer):
-    # format the message
-    mydict = {}
-    mydict["message"] = 2
-    # number of files
-    mydict["length"] = len(fileDict.keys())
-    mydict["files"] = {}
-    for key in fileDict:
-        mydict["files"][key] = fileDict.get(key)
-    y = json.dumps(mydict)
-    conn.sendto(str.encode(y),peer)
+    # add the message
+    # send it to all the other servers
+    # send a message back to the client saying it was sent to all the servers
+
+# send a message back to the client saying that it was sent to all the connections (allConn)
+def write_to_client(conn,addr,sender, comment,timestamp,success):
+    msgdict = {
+        "message": 1,
+        "sender": sender,
+        "comment": comment,
+        "timestamp": timestamp,
+        "success": 1
+    }
+    y = json.dumps(msgdict)
+    conn.sendto(str.encode(y), addr)
     return 0
 
-# message #3
-# Requested: The IP end-points of the peers containting the requested file
-def file_locations_request(conn, peer, filename):
-    if filename in fileDict:
-        # gets the peers and the chunks
-        newdict = {
-            "message": 3,
-            "fileName" : filename,
-            "peers": fileDict.get(filename)
-        }
-        # the number of endpoints
-        newdict["length"] = len(fileDict.get(filename).keys())
-        y = json.dumps(newdict)
-        conn.sendto(str.encode(y),peer)
-        return 0
-    else:
-        return -1
-    # else none if the filename doesnt exist?
 
-# message #4
-# The server is being notified when a peer receives a new chunk of the file and becomes a source 
-# (of that chunk) for other peers
-def chunk_register_request(conn, peer, pFile, chunk, lPort):
-    peerKey = str(peer[0])+":"+str(lPort)
-    if peerKey in fileDict[pFile].keys():
-        fileDict[pFile][peerKey].append(chunk)
-        fileDict[pFile][peerKey] = list(set(fileDict[pFile][peerKey]))
-    else: 
-        fileDict[pFile][peerKey] = [chunk]
-    conn.sendto(str.encode(json.dumps({"message":4, "file":pFile, "chunk":chunk, "success":1})), peer)
+# the client is requesting all the messages
+def request_all_comments(conn,addr):
+    msgdict = {
+        "message": 2,
+        "comment": allmessages,
+    }
+    y = json.dumps(msgdict)
+    conn.sendto(str.encode(y),addr)
     return 0
 
+#def sort_dict(mydict):
+
+#    new_list = sorted(mydict.items(), key=lambda x:x[0])
+#    mydict = dict(new_list)
 
 # handles requests from the individual client within a thread
 def handle_conn(conn, peer):
+    global allmessages
+
     data = conn.recv(4096).decode()
     data = json.loads(data)
     message = data["message"]
-    print(data)
+
+    mutex.acquire()
+    # client wants to send a message to the server for all servers
     if message == 1:
-        files = []
-        for pFile in data["files"]:
-            #  each chunk is 1024 bytes
-            files.append((pFile[0], math.ceil(pFile[1] / 1024)))
-        register_request(conn, peer, files, data["lPort"])
-    
+        sender = data["sender"]
+        comment = data["comment"]
+        timestamp = data["timestamp"]
+        # format a dictionary to append to the messages list {"timestamp" : (sender,message)}
+        allmessages[timestamp] = (sender,comment)
+        # sort the dictionary
+        new_list = sorted(allmessages.items(), key=lambda x:x[0])
+        allmessages = dict(new_list)
+        print("this is all messages: " + str(allmessages))
+
+        # Check if there are any servers connected before trying to send out the message
+        if len(allConn)==0:
+            success = -1
+            # write to the client that there is no one to send it to.
+            # with a message like no one to send it to
+            write_to_client(conn,peer,sender, comment,timestamp,success)
+        else:
+            # loop through conn list and start a new thread and send the message
+            for server in allConn:
+                #print("this is server:" + str(server))
+                servSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                servSock.connect(('127.0.0.1', int(server)))
+                start_new_thread(write_to_server, (servSock,sender,comment,timestamp))
+            # tell the client that it was successful
+            success = 0
+            write_to_client(conn,peer,sender, comment,timestamp,success)
+
     if message == 2:
-        file_list_request(conn, peer)
-    
-    if message == 3:
-        filename = data["fileName"]
-        file_locations_request(conn, peer, filename)
+        #servSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #servSock.connect(('127.0.0.1', int(server)))
+        request_all_comments(conn,peer)
         
-    if message == 4:
-        lPort = data["lPort"]
-        pFile = data["fileName"]
-        chunk = data["chunk"]
-        chunk_register_request(conn, peer, pFile, chunk, lPort)
+
+    # client wants to see all the messages ever made
+    #if message == 2:
+    #    request_all_comments(conn,peer)
+
+    # server is receiving a message
+    if message == 3:
+        sender = data["sender"]
+        comment = data["comment"]
+        timestamp = data["timestamp"]
+        # add the message into allmessages
+        allmessages[timestamp] = (sender,comment)
+        # sort the dictionary
+        new_list = sorted(allmessages.items(), key=lambda x:x[0])
+        allmessages = dict(new_list)
+        print("these are all the messages: " + str(allmessages))
+        
+    mutex.release()
+
     conn.close()
+
     
-# a dictionary where the key is the file name. Each file name has a dictionary. in that dictionary it will have the peer as a key 
-# and a list of chunks it has. the peer will be a tuple of ip addr and port
-# ex: {filename: {peer1: [1,3,5,7,8]}} 
-fileDict = {}
+# a list of all server connections, tuple(conn,addr)
+allConn = []
+# a dictionary of all messages key = timestamp value: sender, and comment
+allmessages = {}
+# a lock for multithreading
+mutex = Lock()
+
+# the command line will be:
+# $ python3 server.py server1 65000
+
 
 # localhost
 HOST = '127.0.0.1'  
 # Port to listen on    
-PORT = int(sys.argv[1])
+PORT = int(sys.argv[2])
     
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST,PORT))
@@ -132,5 +144,15 @@ sock.listen()
 while True:
     conn, addr = sock.accept()
     print("Connected by ", addr)
+    who = sys.argv[1]
+    # can just have server.py
+    for i in range(3,len(sys.argv)):
+        allConn.append(sys.argv[i])
+    allConn = list(set(allConn))
+    print("this is the connections: " + str(allConn))
+
+    # already handles server-to-server and server-to-client connections
+    # this will be listening and receive messages already
     start_new_thread(handle_conn, (conn, addr))
 sock.close()
+ 
